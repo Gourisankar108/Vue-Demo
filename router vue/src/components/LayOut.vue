58c014d0546d72b777d4abf457dfccfbcae47e27
Gourisankar108 <Gourisankar.Sahu@programmers.io>
2024-04-02 16:14:49 +0530

    routervue removed

--- router vue/src/components/LayOut.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <ul class="nav navbar-nav">
-        <li class="active"><router-link to="/">Home</router-link></li>
-        <li><router-link to="/hello">Hello World</router-link></li>
-        <li><router-link to="/product">Product</router-link></li>
-    </ul>
-    <hr>
-    <router-view></router-view>
-</template>
--- router vue/src/components/ProductList.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>Product Page</h1>
-</template>
-
-<script setup>
-
-</script>
-
-
