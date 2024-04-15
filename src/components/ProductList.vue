9e8cb18bb7d2608fcaf74c5963578cb06440d1fd
Gourisankar108 <Gourisankar.Sahu@programmers.io>
2024-04-15 18:51:35 +0530

    Axios APi call

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,14 +1,43 @@
 <template>
     <div style="border: 1px solid;padding: 15px;width: 50%;margin: auto;">
         <h1>Product Page Data Transfer</h1>
-    {{ auth }}
-
+        {{ auth }}
     </div>
+
+    <ul>
+        <li v-for="(prod, index) in productList" :key="prod.id">
+            {{ prod.title }} - {{ index }}
+        </li>
+    </ul>
 </template>
 
 <script setup>
-import {defineProps} from 'vue'
+import axios from 'axios';
+import { defineProps, onMounted, ref } from 'vue'
 
 const auth = defineProps({ 'authorData': Object });
+
+const productList = ref([]);
+onMounted(async () => {
+    getAllProducts();
+})
+
+async function getAllProducts() {
+    const url = `https://dummyjson.com/products`;
+    await axios.get(url).then(response => {
+        const _allProducts = JSON.parse(JSON.stringify(response.data.products));
+        console.log("_allProducts", _allProducts)
+        _allProducts.sort((a, b) => {
+            if (a.id < b.id) {
+                return -1;
+            }
+            if (a.id > b.id) {
+                return 1;
+            }
+            return 0;
+        });
+        productList.value = _allProducts;
+    })
+}
 
 </script>
